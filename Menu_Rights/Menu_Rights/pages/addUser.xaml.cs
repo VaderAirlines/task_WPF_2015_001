64539878794890f955a8ba6e3b1f140a7b2ea934
VaderAirlines <dennisbellinkx@gmail.com>
2015-11-12 22:58:17 +0100

    tried to create testform with databinding

--- Menu_Rights/Menu_Rights/pages/addUser.xaml.cs
+++ Menu_Rights/Menu_Rights/pages/addUser.xaml.cs
@@ -17,8 +17,44 @@ namespace Menu_Rights.pages {
     /// Interaction logic for addUser.xaml
     /// </summary>
     public partial class addUser:Page {
+
+        public List<lowieke> testList = null;
+        public testObject testObject = new testObject() { testProperty="joske" };
+
         public addUser() {
             InitializeComponent();
+
+            testList = new List<lowieke>() { new lowieke() { joske="eerste dinges", agnes = false},
+                                             new lowieke() { joske="eerste dinges", agnes = false},
+                                             new lowieke() { joske="eerste dinges", agnes = false},
+                                             new lowieke() { joske="eerste dinges", agnes = false},
+                                             new lowieke() { joske="eerste dinges", agnes = false},
+                                             new lowieke() { joske="eerste dinges", agnes = false},
+                                             new lowieke() { joske="eerste dinges", agnes = false},
+                                             new lowieke() { joske="tweede dinges", agnes=true} 
+                                            };
+
+            cmbGroups.DataContext = testList; 
+            txtLogin.DataContext = testObject;
         }
+
+        private void createUser(object sender,RoutedEventArgs e) {
+            if (cmbGroups.SelectedIndex != -1) {
+                lowieke item = (lowieke)cmbGroups.SelectedItem;
+                MessageBox.Show(item.joske);
+                MessageBox.Show(testObject.testProperty);
+            } else {
+                MessageBox.Show("Please choose a group.");
+            }
+        }
+    }
+
+    public class lowieke {
+        public string joske { get; set; }
+        public bool agnes { get; set; }
+    }
+
+    public class testObject {
+        public string testProperty { get; set; }
     }
 }
